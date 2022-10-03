f4958adb755f841586bb3501cb3b8d52ebc130cf
Oluwafemi Martins <oluwafemi@DESKTOP-1TB3K83.localdomain>
2022-10-03 16:50:48 +0100

    create file for all variables of the resources

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "cidr_block" {
+  type = string
+}
+
+variable "cidr_block_pub1" {
+  type = string
+}
+
+variable "cidr_block_pub2" {
+  type = string
+}
+
+variable "cidr_block_pri1" {
+  type = string
+}
+
+variable "cidr_block_pri2" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+
+
+
+
